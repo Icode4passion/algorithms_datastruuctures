c371206c3e97ed90c1d8377c645369a033a9e242
Icode4passion <39987888+Icode4passion@users.noreply.github.com>
2020-06-06 02:51:04 +0530

    Create SingleLinkedList.py

---
+++ SingleLinkedList.py
@@ -0,0 +1,87 @@
+class Node :
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.count = 0
+    def addNode(self,data):
+        node = Node(data)
+        self.count +=1
+        
+        if not self.head:
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+    def display(self):
+        current = self.head
+
+        if self.head is None:
+            print("List is Empty")
+        while current is not None:
+            print(current.data , end="" + "->")
+            current = current.next
+        print()
+
+    def insertAtstart(self,data):
+        node = Node(data)
+        if not self.head:
+            self.head=node
+            self.tail = node
+        else:
+            node.next = self.head
+            self.head = node
+
+    def countLinkedList(self):
+        count = 0
+        current = self.head
+        while current is not None:
+            count +=1
+            current = current.next
+        return count
+
+    def countInLinkedList(self):
+        return self.count
+
+    def insertAtEnd(self,data):
+        node = Node(data)
+        if self.head is None:
+            self.head = node
+            return
+        current = self.head
+        while current.next is not None:
+            current = current.next
+        current.next = node
+        
+            
+        
+            
+
+        
+ll =  LinkedList()
+
+ll.addNode(10)
+ll.addNode(20)
+ll.addNode(30)
+ll.addNode(40)
+ll.insertAtstart(1)
+ll.insertAtEnd(11)
+ll.insertAtEnd(21)
+
+
+ll.display()
+print("Below count prints all the nodes values, since it loops in all the nodes Time complexity is O(N)")
+print("Count" ,ll.countLinkedList())
+print("Below Count prints Nodes that are inserted in the addNode method but not in the insertAtStart its time Complexity is O(1)")
+print("Count" ,ll.countInLinkedList())
+
+
+
+
+
